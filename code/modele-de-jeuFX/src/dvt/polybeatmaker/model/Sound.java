6d0baa3c3ff55ba2fbe0b7486d9c313cc8d42855
Charlotte Aknin <charlotte.aknin@etu.unice.fr>
2017-04-05 14:07:38 +0200

    updated maingame design

--- code/modele-de-jeuFX/src/dvt/polybeatmaker/model/Sound.java
+++ code/modele-de-jeuFX/src/dvt/polybeatmaker/model/Sound.java
@@ -1,13 +1,22 @@
 package dvt.polybeatmaker.model;
 
+import javafx.scene.control.Button;
+
+import java.util.Timer;
+
 /**
  * Single sound sample.
  */
 public class Sound {
-
-    public Sound() {
-
+    Timer timer;
+    public Sound()
+    {
+        timer = new Timer();
     }
 
+    public boolean toBePlayed(Button b)
+    {
+        return true;
+    }
 
 }
