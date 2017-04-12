adbbda10220dac44bfcacba23ecb576b84669178
Guillaume André <guillaume.andre@etu.unice.fr>
2017-04-12 15:04:46 +0200

    change buttons into togglebuttons

--- code/modele-de-jeuFX/src/dvt/polybeatmaker/controller/InstrumentController.java
+++ code/modele-de-jeuFX/src/dvt/polybeatmaker/controller/InstrumentController.java
@@ -5,7 +5,7 @@ import dvt.polybeatmaker.model.PolybeatModel;
 import dvt.polybeatmaker.model.Sound;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -24,11 +24,11 @@ public class InstrumentController {
     @FXML private ImageView picture;
 
     private Instrument instrument;
-    private List<Button> buttons;
+    private List<ToggleButton> buttons;
     private PolybeatModel model;
 
     private Sound activated;
-    private Button activatedButton;
+    private ToggleButton activatedButton;
 
     public void setInstrument(Instrument instrument) {
         this.instrument = instrument;
@@ -41,12 +41,12 @@ public class InstrumentController {
     public void init() throws MalformedURLException {
         buttons = new ArrayList<>();
         for (Node c : box1.getChildren()) {
-            buttons.add((Button) c);
+            buttons.add((ToggleButton) c);
         }
         for (Node c : box2.getChildren()) {
-            buttons.add((Button) c);
+            buttons.add((ToggleButton) c);
         }
-        for (Button b : buttons) {
+        for (ToggleButton b : buttons) {
             b.setStyle(instrument.getStyle());
             b.setOnMouseClicked(event -> {
                 if (activated != null) {
